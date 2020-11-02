fca82143b2019c981412a0efc12b0b4a636d76b3
Akash Dutta <dutta.akash1993official@gmail.com>
2020-11-02 11:13:23 +0530

    product, category, order schema updated

--- projbackend/app.js
+++ projbackend/app.js
@@ -1,24 +1,49 @@
-require('dotenv').config();
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
-const cors = require('cors');
+const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
+// const bodyParser = require('body-parser');
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
 
 const app = express();
 
 // Middlewares
-app.use(bodyParser.json());
-app.use(cookieParser());
 app.use(cors());
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
-// // My routes
+app.use(express.json());
+app.use(cookieParser());
+
+// // DB Connection
+mongoose
+  .connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then(() => {
+    console.log("DB CONNECTED");
+  })
+  .catch(() => {
+    console.log("PROBLEM CONNECTING DB");
+  });
+
+
+// My routes
 // const authRoute = require('./routes/authRoute');
 // const userRoute = require('./routes/userRoute');
 // const categoryRoute = require('./routes/categoryRoute');
 // const productRoute = require('./routes/productRoute');
 // const orderRoute = require('./routes/orderRoute');
+
 
 // // Routers
 // app.use('/api/',authRoute);
@@ -28,21 +53,9 @@ app.use(cors());
 // app.use('/api/', orderRoute);
 
 
-// // DB Connection
-// mongoose.connect(process.env.DATABASE, {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true, 
-//     useCreateIndex: true    
-// }).then(() => {
-//     console.log("DB CONNECTED");
-// }).catch(() => {
-//     console.log("PROBLEM CONNECTING DB");
-// })
-
 // PORT
 const port = process.env.PORT || 8080;
 // Starting server
 app.listen(port, () => {
-    console.log(`App is running at port ${port}`);
+  console.log(`App is running at port ${port}`);
 });
-
