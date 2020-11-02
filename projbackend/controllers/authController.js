2da57f55a03c950faf133d2b7536c5ad45e561bd
Akash Dutta <dutta.akash1993official@gmail.com>
2020-11-02 20:47:31 +0530

    sign up, login, signout updated

--- projbackend/controllers/authController.js
+++ projbackend/controllers/authController.js
@@ -29,60 +29,60 @@ exports.signup = (req, res) => {
   });
 };
 
-// // LOGIN/SIGNIN
-// exports.login = (req, res) => {
-//   const errors = validationResult(req);
-//   const { email, password } = req.body;
+// LOGIN/SIGNIN
+exports.login = (req, res) => {
+  const errors = validationResult(req);
+  const { email, password } = req.body;
 
-//   if (!errors.isEmpty()) {
-//     return res.status(422).json({
-//       error: errors.array()[0].msg,
-//     });
-//   }
+  if (!errors.isEmpty()) {
+    return res.status(422).json({
+      error: errors.array()[0].msg,
+    });
+  }
 
-//   // to find email and password in DB
-//   User.findOne({ email }, (err, user) => {
-//     if (err || !user) {
-//       return res.status(400).json({
-//         error: "Email dose not exists",
-//       });
-//     }
+  // to find email and password in DB
+  User.findOne({ email }, (err, user) => {
+    if (err || !user) {
+      return res.status(400).json({
+        error: "Email dose not exists",
+      });
+    }
 
-//     if (!user.authenticate(password)) {
-//       return res.status(402).json({
-//         err: "Email or password do not match",
-//       });
-//     }
+    if (!user.authenticate(password)) {
+      return res.status(402).json({
+        err: "Email or password do not match",
+      });
+    }
 
-//     // create token
-//     const token = jwt.sign({ _id: user._id }, process.env.SECRET);
-//     // put token in cookie
-//     res.cookie("token", token, { expire: new Date() + 9999 });
+    // create token
+    const token = jwt.sign({ _id: user._id }, process.env.SECRET);
+    // put token in cookie
+    res.cookie("token", token, { expire: new Date() + 9999 });
 
-//     // send response to front end
-//     const { _id, firstName, lastName, email, mobileNumber, role } = user;
-//     return res.json({
-//       token,
-//       user: {
-//         _id,
-//         firstName,
-//         lastName,
-//         email,
-//         mobileNumber,
-//         role,
-//       },
-//     });
-//   });
-// };
+    // send response to front end
+    const { _id, firstName, lastName, email, mobileNumber, role } = user;
+    return res.json({
+      token,
+      user: {
+        _id,
+        firstName,
+        lastName,
+        email,
+        mobileNumber,
+        role,
+      },
+    });
+  });
+};
 
 // // SIGNOUT
-// exports.signout = (req, res) => {
-//   res.clearCookie("token");
-//   res.status(200).json({
-//     status: "Success",
-//     message: "You signed out successfully",
-//   });
-// };
+exports.signout = (req, res) => {
+  res.clearCookie("token");
+  res.status(200).json({
+    status: "Success",
+    message: "You signed out successfully",
+  });
+};
 
 // // PROTECTED ROUTES MIDDLEWARES
 
--- projbackend/routes/authRoute.js
+++ projbackend/routes/authRoute.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const { check, validationResult } = require('express-validator');
-const {signup, } = require('../controllers/authController');
+const {signup, login, signout} = require('../controllers/authController');
 
-// login, signout, isSignedIn
+// , , isSignedIn
 router.post('/signup', [
     check("firstName")
     .isLength({ min: 2 }).withMessage('Firstname must have at least 3 chars long'),
@@ -22,16 +22,16 @@ router.post('/signup', [
     .matches(/\d/).withMessage('Password must contain a number')
 ], signup);
 
-// router.post('/login', [    
-//     check("email")
-//     .isEmail().withMessage('Must be a valid email id'),
+router.post('/login', [    
+    check("email")
+    .isEmail().withMessage('Email is required'),
 
-//     check("password")
-//     .isLength({min:8}).withMessage('Must be a atleast 8 character long')
-//     .matches(/\d/).withMessage('must contain a number')
-// ], login);
+    check("password")
+    .isLength({min:8}).withMessage('Enter valid password')
+    .matches(/\d/).withMessage('Enter valid password')
+], login);
 
-// router.get('/signout', signout);
+router.get('/signout', signout);
 
 // router.get('/test', isSignedIn, (req, res) => {
 //     res.json(req.auth)
