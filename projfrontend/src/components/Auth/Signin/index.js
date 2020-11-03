d910de0238beb0ec093d0b17b5b742d8ce98eca9
Akash Dutta <dutta.akash1993official@gmail.com>
2020-11-04 01:41:20 +0530

    login  done

--- projfrontend/src/components/Auth/Signin/index.js
+++ projfrontend/src/components/Auth/Signin/index.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
+import { Link, Redirect } from "react-router-dom";
 import Button from "../../Button";
-import { signIn } from "../api";
+import { signIn, authenticate, isAuthenticated } from "../api";
 import Swal from "sweetalert2";
 
 const Signin = () => {
@@ -8,25 +9,71 @@ const Signin = () => {
     email: "",
     password: "",
     error: "",
-    success: false,
+    loading: false,
+    didRedirect: false,
   });
 
-  const { email, password, error, success } = value;
+  const { email, password, error, loading, didRedirect } = value;
+  const { user } = isAuthenticated();
 
   const handleChange = (name) => (event) => {
     setValue({ ...value, error: false, [name]: event.target.value });
   };
 
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setValue({ ...value, error: false, loading: true });
+    signIn({ email, password })
+      .then((data) => {
+        if (data.error) {
+          setValue({ ...value, error: data.error, success: false });
+          Swal.fire({
+            icon: "error",
+            title: "Oops...",
+            text: data.error,
+          });
+        } else {
+          authenticate(data, () => {
+            setValue({
+              ...value,
+              didRedirect: true,
+              email: "",
+              password: "",
+            });
+          });
+          Swal.fire("Success!", "User loggedin successfully!", "success");
+        }
+      })
+      .catch((err) => console.log("From sign in page", err));
+  };
+
+  const isLoading = () => {
+    return (
+      loading && (
+        <div>
+          <h2>Loading...</h2>
+        </div>
+      )
+    );
+  };
+  const doRedirect = () => {
+    if (didRedirect) {
+      if (user && user.role === 1) {
+        return <p>Redirect to admin dashboard</p>;
+      } else {
+        return <p>Redirect to user dashboard</p>;
+      }
+    }
+    if (isAuthenticated()) {
+      return <Redirect to="/" />;
+    }
+  };
   const signInForm = () => {
     return (
       <div>
         <form>
           <label>Email:</label>
-          <input 
-            type="email" 
-            onChange={handleChange("email")} 
-            value={email} 
-          />
+          <input type="email" onChange={handleChange("email")} value={email} />
 
           <label>Password:</label>
           <input
@@ -35,11 +82,17 @@ const Signin = () => {
             value={password}
           />
         </form>
-        <Button value="Log In" />
+        <Button value="Log In" onClick={handleSubmit} />
       </div>
     );
   };
-  return <div>{signInForm()}</div>;
+  return (
+    <div>
+      {isLoading()}
+      {signInForm()}
+      {doRedirect()}
+    </div>
+  );
 };
 
 export default Signin;
