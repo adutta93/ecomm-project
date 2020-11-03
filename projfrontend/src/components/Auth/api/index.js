80be2741972fdbac37cfbbe87acbb70e2804ba14
Akash Dutta <dutta.akash1993official@gmail.com>
2020-11-03 21:48:49 +0530

    some update on sign in and sign out and api added

---
+++ projfrontend/src/components/Auth/api/index.js
@@ -0,0 +1,67 @@
+import { API } from "../../../backendAPI";
+
+//signup
+export const signUp = (user) => {
+  return fetch(`${API}/signup`, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(user),
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .catch((err) => console.log(err));
+};
+
+//signin
+export const signUp = (user) => {
+    return fetch(`${API}/login`, {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(user),
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .catch((err) => console.log(err));
+  };
+  
+
+//authenticate
+export const authenticate = (data, next) => {
+  if (typeof window !== "undefined") {
+    localStorage.setItem("jwt", JSON.stringify(data));
+    next();
+  }
+};
+//signout
+export const signOut = (next) => {
+  if (typeof window !== "undefined") {
+    localStorage.removeItem("jwt");
+    next();
+
+    return fetch(`${API}/signout`, {
+      method: "GET",
+    })
+      .then((response) => console.log("Signout success"))
+      .catch((err) => console.log(err));
+  }
+};
+
+//check if its authenticated
+export const isAuthenticated = () => {
+  if (typeof window == "undefined") {
+    return false;
+  }
+  if (localStorage.getItem("jwt")) {
+    return JSON.parse(localStorage.getItem("jwt"));
+  } else {
+    return false;
+  }
+};
